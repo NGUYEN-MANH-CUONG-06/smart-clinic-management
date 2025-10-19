f4915c196d6c4d2f1e8c34f7ec212c4774db7318
NGUYEN-MANH-CUONG-06 <manhcuong6524@gmail.com>
2025-10-19 10:50:42 +0700

    Update AppointmentService.java

--- app/src/main/java/com/project/backend/services/AppointmentService.java
+++ app/src/main/java/com/project/backend/services/AppointmentService.java
@@ -2,25 +2,21 @@ package com.project.back_end.services;
 
 import com.project.back_end.models.Appointment;
 import com.project.back_end.repositories.AppointmentRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.time.LocalDate;
-import java.util.List;
 
 @Service
 public class AppointmentService {
 
-    @Autowired
-    private AppointmentRepository appointmentRepository;
+    private final AppointmentRepository appointmentRepository;
 
-    public List<Appointment> getAppointmentsByDoctorAndDate(Long doctorId, LocalDate date) {
-        return appointmentRepository.findByDoctorIdAndAppointmentDateBetween(
-                doctorId,
-                date.atStartOfDay(),
-                date.plusDays(1).atStartOfDay()
-        );
+    public AppointmentService(AppointmentRepository appointmentRepository) {
+        this.appointmentRepository = appointmentRepository;
+    }
+
+    public Appointment bookAppointment(Appointment appointment) {
+        return appointmentRepository.save(appointment);
     }
 }
+
 
 
